a0185e568b35f3284cefdeaaf53e87c3d1bc8bfa
ElecarnoYT <benrobmc.mc@gmail.com>
2024-05-19 17:20:58 +0100

    opening animation

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var jingleButton = document.getElementById("jingle-button")
+var enterButton = document.getElementById("enter-button")
 
 var jingle = new Audio("./audio/portfolio_website_jingle_2.mp3");
+
+enterButton.addEventListener("click", function() { 
+    const quavers = document.querySelectorAll('.quaver');
+    quavers.forEach(quaver => {
+        quaver.style.animationPlayState = 'running';
+    });
+
+    jingle.play();
+})
 
 jingleButton.addEventListener("click", function() { 
     jingle.play();
 })
+
 
 feather.replace()
