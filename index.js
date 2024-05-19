4c78902fbf914e464da5415906bccbfe6ef3e9e9
ElecarnoYT <benrobmc.mc@gmail.com>
2024-05-19 15:59:29 +0100

    jingle audio player

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-// document.addEventListener("mousemove", parallax);
+var jingleButton = document.getElementById("jingle-button")
 
-// function parallax(e) {
-//     const bgImage = document.querySelector("#bg-image");
-//     if (bgImage) {
-//         let _w = window.innerWidth / 2;
-//         let _h = window.innerHeight / 2;
-//         let _mouseX = e.clientX;
-//         let _mouseY = e.clientY;
-//         // Apply easing equation (example: easeOutQuad)
-//         let easedX = easeOutQuad(_mouseX - _w, 0, 1, window.innerWidth / 2);
-//         let easedY = easeOutQuad(_mouseY - _h, 0, 1, window.innerHeight / 2);
-//         bgImage.style.backgroundPosition = `
-//             calc(50% - ${easedX * 20}px) calc(50% - ${easedY * 20}px)
-//         `;
-//     }
-// }
+var jingle = new Audio("./audio/portfolio_website_jingle_2.mp3");
 
-// // Easing function: Quadratic easing out (https://easings.net/en)
-// function easeOutQuad(t, b, c, d) {
-//     t /= d;
-//     return -c * t * (t - 2) + b;
-// }
+jingleButton.addEventListener("click", function() { 
+    jingle.play();
+})
+
+feather.replace()
